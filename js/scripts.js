5bb64d37b654a6ff72b3aa3a2b98411d5f04dacf
Alaa Batayneh <a.batayneh@gmail.com>
2017-05-22 13:22:19 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 });
 
 function init() {
-  var google_sheet_url = 'https://docs.google.com/spreadsheets/d/1Hw4ULimeChPvc6EC03238rRAxabNMQfVe8bs13ab66k/pubhtml';
+  var google_sheet_url = 'https://docs.google.com/spreadsheets/d/12CSYHWSxXwUvfpoUaKjQNkcx88WcCwqovs_-Cg45Vrw/pubhtml';
   Tabletop.init({
     key: google_sheet_url,
     callback: play
@@ -18,13 +18,14 @@ function play(data, tabletop) {
   title = data.Sheet2.elements[0].Title;
   desc = data.Sheet2.elements[0].Description
   source_name = data.Sheet2.elements[0].Source
-  source_link = data.Sheet2.elements[0].Link
   canonical = data.Sheet2.elements[0].Canonical
   twitter = data.Sheet2.elements[0].Twitter
+  highest = data.Sheet2.elements[0].HighestOnTop
+  conc = data.Sheet2.elements[0].Conclusion
 
   $('#main-title').html(title)
   $('#main-description').html(desc)
-  $('.source-name').html('<a target="_blabk" href="' + source_link + '">' + source_name + '</a>')
+  $('a.source-link').attr('href', source_name)
   $('.rrssb-facebook a').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + canonical)
   $('.rrssb-twitter a').attr('href', 'https://twitter.com/intent/tweet?text=' + twitter + ' ' + canonical)
 
@@ -34,13 +35,16 @@ function play(data, tabletop) {
   });
 
   items_to_load = data.Sheet2.elements[0].Items
-  var winning = get_list(items_to_load, unique_countries);
+  var winning = get_list(highest, items_to_load, unique_countries, conc);
   var movinglist = document.getElementById('items');
   var sortable = new Sortable(movinglist, {
     draggable: '.item',
     animation: 150,
     onEnd: function(evt) {
-
+      correct = $('.correct:visible').length;
+      if (parseInt(correct) === parseInt(items_to_load)) {
+        $('.conc').fadeIn()
+      }
     },
     onUpdate: function(evt, winning) {
       // update attempls
@@ -91,11 +95,20 @@ function moveDown($item) {
 }
 
 
-function get_list(num, countries) {
-  rand   = _.sampleSize(countries, num)
-  sorted = _.sortBy(rand, [function(o) { return parseInt(o.score); }]);
-  for (var x = 0; x < rand.length; x++) {
-    key = _.findKey(sorted, ['country', rand[x].country])
-    $("#items").append("<li class='item' data-rank='" +  key + "' data-score='" + rand[x].score + "'><span class='drag-handle'>☰</span>" + rand[x].country + "<span><span class='conc'> - employment rate is " + rand[x].score  + "</small></span><img class='correct' src='img/tick.png' /></li>")
+function get_list(highest, num, countries, conc) {
+  if (highest === 'yes') {
+    rand = _.sampleSize(countries, num)
+    sorted = _.sortBy(rand, [function(o) { return parseInt(o.score); }]).reverse();
+    for (var x = 0; x < rand.length; x++) {
+      key = _.findKey(sorted, ['country', rand[x].country])
+      $("#items").append("<li class='item' data-rank='" +  key + "' data-score='" + rand[x].score + "'><span class='drag-handle'>☰</span>" + rand[x].country + "<span><span class='conc'> -  " + conc + ' ' + rand[x].score  + "</small></span><img class='correct' src='img/tick.png' /></li>")
+    }
+  } else {
+    rand   = _.sampleSize(countries, num)
+    sorted = _.sortBy(rand, [function(o) { return parseInt(o.score); }]);
+    for (var x = 0; x < rand.length; x++) {
+      key = _.findKey(sorted, ['country', rand[x].country])
+      $("#items").append("<li class='item' data-rank='" +  key + "' data-score='" + rand[x].score + "'><span class='drag-handle'>☰</span>" + rand[x].country + "<span><span class='conc'> -  " + conc + ' ' + rand[x].score  + "</small></span><img class='correct' src='img/tick.png' /></li>")
+    }
   }
 }
